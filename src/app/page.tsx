149baec6dc061e193052766dc3eb089f774624e1
Pushkar Dutta <pushkardutt555@gmail.com>
2024-09-12 14:56:30 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-black p-12">
+      <video
+        src="/asset/sale-vid.mp4"
+        autoPlay
+        muted
+        controls
+        loop
+        className="w-full sm:w-[50%] max-w-4xl"
+      />
+    </div>
+  );
+}
